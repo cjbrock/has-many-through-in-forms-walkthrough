34825ff242c87940d0a3baeeace9a38dcc73aeb8
cjbrock <corinna@flatironschool.com>
2020-11-01 10:58:26 -0500

    add post study group code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_atts)
+    self.user = User.find_or_create_by(username: user_atts[:username]) unless user_atts[:username].blank?
+  end
+
+  # def user_attributes=(user_atts)
+  #   if !user_atts[:username].blank?
+  #     self.user = User.find_or_create_by(username: user_atts[:username]
+  #   end
+  # end
+
 
 end
